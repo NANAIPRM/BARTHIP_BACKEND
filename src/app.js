fc3383a5e73b6307ecc383c772f089dd286d740d
porramat <>
2023-06-20 12:18:23 +0700

    update .env.example & prettier

--- src/app.js
+++ src/app.js
@@ -1,33 +1,33 @@
-require("dotenv").config();
-const express = require(".pnpm/express@4.18.2/node_modules/express");
-const cors = require(".pnpm/cors@2.8.5/node_modules/cors");
-const morgan = require(".pnpm/morgan@1.10.0/node_modules/morgan");
-const helmet = require("helmet");
-const rateLimit = require("express-rate-limit");
+require('dotenv').config()
+const express = require('.pnpm/express@4.18.2/node_modules/express')
+const cors = require('.pnpm/cors@2.8.5/node_modules/cors')
+const morgan = require('.pnpm/morgan@1.10.0/node_modules/morgan')
+const helmet = require('helmet')
+const rateLimit = require('express-rate-limit')
 
-const notFoundMiddleware = require("./middlewares/notFound");
-const errorMiddleware = require("./middlewares/error");
+const notFoundMiddleware = require('./middlewares/notFound')
+const errorMiddleware = require('./middlewares/error')
 
-const app = express();
+const app = express()
 
-if (process.env.NODE_ENV === "develop") {
-  app.use(morgan("combined"));
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('combined'))
 }
 
 app.use(
-  rateLimit({
-    windowMs: 1000 * 60 * 15,
-    max: 1000,
-    message: { message: "too many request" },
-  })
-);
-app.use(helmet());
-app.use(cors());
-app.use(express.json());
+    rateLimit({
+        windowMs: 1000 * 60 * 15,
+        max: 1000,
+        message: { message: 'too many request' },
+    })
+)
+app.use(helmet())
+app.use(cors())
+app.use(express.json())
 
-app.use(notFoundMiddleware);
-app.use(errorMiddleware);
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8000
 
-app.listen(port, () => console.log("server running in port " + port));
+app.listen(port, () => console.log('server running in port ' + port))
--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -1,8 +1,8 @@
 module.exports = (err, req, res, next) => {
-  console.log(err);
+    console.log(err)
 
-  if (err.name === "ValidationError") {
-    err.statusCode = 400;
-  }
-  res.status(err.statusCode || 500).json({ message: err.message });
-};
+    if (err.name === 'ValidationError') {
+        err.statusCode = 400
+    }
+    res.status(err.statusCode || 500).json({ message: err.message })
+}
