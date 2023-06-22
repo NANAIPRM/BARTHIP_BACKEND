438aaf5024d35c407228047528eb21b9874287a9
Krittayot007 <krittayot.som@gmail.com>
2023-06-22 17:51:37 +0700

    add feature google but not add route

--- src/controllers/google-controller.js
+++ src/controllers/google-controller.js
@@ -1,6 +1,7 @@
 const createError = require('../utils/create-error')
 const tokenService = require('../services/token-service')
 const jwtDecode = require('jwt-decode')
+const User = require('../models')
 
 exports.googleLogin = async (req, res, next) => {
     try {
@@ -8,6 +9,22 @@ exports.googleLogin = async (req, res, next) => {
         const isToken = await tokenService.verifyGoogle(credential)
         if (!isToken) throw new createError('Invalid Google-Token ', 401)
         const googleUser = jwtDecode(credential)
+        const user = await User.findOne({
+            where: {
+                email: googleUser.email,
+            },
+        })
+        let newUser
+        if (!user) {
+            newUser = await User.create({
+                email: googleUser.email,
+                googleToken: credential,
+            })
+        }
+        const token = user
+            ? tokenService.sign({ id: user.id })
+            : tokenService.sign({ id: newUser.id })
+        res.status(200).json({ token })
     } catch (err) {
         next(err)
     }
